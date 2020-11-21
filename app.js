0d1ceb6105d20b17ba3816ab05ce48cef24d9086
matespinosa <matespinosa09@gmail.com>
2020-11-21 17:33:04 -0500

    nada

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const color = document.querySelector('.color');
 btn.addEventListener('click', function () {
   // obtener numero aleatorio entre 0 - 3 colors[3]
   const randomNumber = getRandomNumber();
-  console.log(randomNumber);
 
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber]
